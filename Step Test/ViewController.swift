a1a5d959d932fbe4812af05a74b1f4f20e9763ef
TheSethsquatch <stompvine@gmail.com>
2018-03-07 14:25:36 -0800

    Added contest date range functionality Start Date, End Date and Step Goal are now modifiable for the current contest.

--- Step Test/ViewController.swift
+++ Step Test/ViewController.swift
@@ -13,15 +13,33 @@ import HealthKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var stepCountLabel: UILabel!
-    @IBOutlet weak var monthLabel: UILabel!
-    @IBOutlet weak var dayLabel: UILabel!
-    @IBOutlet weak var dailyGoalLabel: UILabel!
+    @IBOutlet weak var startMonthLabel: UILabel!
+    @IBOutlet weak var startDayLabel: UILabel!
+    @IBOutlet weak var endMonthLabel: UILabel!
+    @IBOutlet weak var endDayLabel: UILabel!
+    @IBOutlet weak var contestGoalLabel: UILabel!
     @IBOutlet weak var dayCardView: UIView!
     @IBOutlet weak var stepProgressBar: UIProgressView!
+   
+    @IBAction func startDateButtonTouched(_ sender: UIButton) {
+        showDateInputDialog(buttonID: "Start")
+    }
+    
+    @IBAction func endDateButtonTouched(_ sender: UIButton) {
+        showDateInputDialog(buttonID: "End")
+    }
+    
+    @IBAction func goalButtonTouched(_ sender: UIButton) {
+        showGoalInputDialog() 
+    }
+    
     
     let storage = HKHealthStore()
     var todayStepCount:Float = 0
-    var dailyGoalStepCount:Float = 10000
+    var contestGoalStepCount:Float = 70000
+    var startDateString = "2018-02-13"
+    var endDateString = "2018-02-15"
+
     
     
     override func viewDidLoad() {
@@ -49,75 +67,73 @@ class ViewController: UIViewController {
         //   Define the Step Quantity Type
         let stepsCount = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
         
-        // TODO: Change to 'start of the match'
-        //   Get the start of the day
-        let date = Date()
-//        var startDate = Date()
-//        let startDateFormatter = DateFormatter()
-//        startDateFormatter.dateFormat = "yyyy-MM-DD"
-//        let startDateString = "2017-01-01"
-//        startDate = startDateFormatter.date(from: startDateString)!
+        
+        //  prepare the predicate
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let startDate = dateFormatter.date(from: startDateString)
+        let endDate = dateFormatter.date(from: endDateString)
+        let endDateForQuery = NSCalendar.current.date(byAdding: .day, value: 1, to: endDate!) // add a day to include the last day
 
-        let cal = Calendar(identifier: Calendar.Identifier.gregorian)
-        let newDate = cal.startOfDay(for: date)
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDateForQuery, options: .strictStartDate)
         
-        //  Set the Predicates & Interval
-        let predicate = HKQuery.predicateForSamples(withStart: newDate, end: Date(), options: .strictStartDate)
-        var interval = DateComponents()
-        interval.day = 1
-        
-        //  Perform the Query
-        let query = HKStatisticsCollectionQuery(quantityType: stepsCount!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: newDate as Date, intervalComponents:interval)
-        
-        query.initialResultsHandler = { query, results, error in
-            
+        let query = HKStatisticsQuery(quantityType: stepsCount!,
+                                      quantitySamplePredicate: predicate,
+                                      options: .cumulativeSum) { query, result, error in
+                                        
             if error != nil {
                 
                 //  Something went Wrong
+                print("Something went wrong")
                 return
             }
-            let now = Date()
-            let startOfDay = Calendar.current.startOfDay(for: now)
 
-            if let myResults = results{
-                myResults.enumerateStatistics(from: startOfDay, to: now) {
-                    statistics, stop in
-                    
-                    if let quantity = statistics.sumQuantity() {
-                        
-                        let steps = quantity.doubleValue(for: HKUnit.count())
-                        
-                        completion(steps)
-                        
-                    }
-                }
+            
+            
+            if let quantity = result?.sumQuantity() {
+                let steps = quantity.doubleValue(for: HKUnit.count())
+                completion(steps)
             }
-            
-            
         }
+
+        
         
         storage.execute(query)
     }
     
     func refreshView() {
+        
         // Update the Calendar labels
-        let todayDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
         
-        let todayMonthFormatter = DateFormatter()
-        todayMonthFormatter.dateFormat = "MMM"
+        let startDate = dateFormatter.date(from: startDateString)
+        let endDate = dateFormatter.date(from: endDateString)
+
         
-        let todayDayFormatter = DateFormatter()
-        todayDayFormatter.dateFormat = "dd"
+        let dateMonthFormatter = DateFormatter()
+        dateMonthFormatter.dateFormat = "MMM"
+        
+        let dateDayFormatter = DateFormatter()
+        dateDayFormatter.dateFormat = "dd"
+        
+        
 
-        let todayDay = todayDayFormatter.string(from: todayDate)
-        let todayMonth = todayMonthFormatter.string(from: todayDate)
+        
+        let startDateMonth = dateMonthFormatter.string(from: startDate!)
+        let startDateDay = dateDayFormatter.string(from: startDate!)
+        let endDateMonth = dateMonthFormatter.string(from: endDate!)
+        let endDateDay = dateDayFormatter.string(from: endDate!)
 
-        monthLabel.text = todayMonth
-        dayLabel.text = todayDay
+        startMonthLabel.text = startDateMonth
+        startDayLabel.text = startDateDay
+        endMonthLabel.text = endDateMonth
+        endDayLabel.text = endDateDay
         
         
         // Update the Daily Goal label
-        dailyGoalLabel.text = makeDecimalString(theNumber: dailyGoalStepCount)
+        contestGoalLabel.text = makeDecimalString(theNumber: contestGoalStepCount)
         
         // Update the Stepcount label
         HealthKitSetupAssistant.authorizeHealthKit { (success, error) in
@@ -128,7 +144,7 @@ class ViewController: UIViewController {
                         self.stepCountLabel.text = self.makeDecimalString(theNumber: Float(Int(stepCount)))
                         self.todayStepCount = Float(stepCount)
                         
-                        self.stepProgressBar.progress = self.todayStepCount / self.dailyGoalStepCount
+                        self.stepProgressBar.progress = self.todayStepCount / self.contestGoalStepCount
                     }
                 }
             }
@@ -146,10 +162,98 @@ class ViewController: UIViewController {
     
      
     @objc func appMovedToForeground() {
-        print("App moved to ForeGround!")
         refreshView()
     }
     
+    deinit {
+        // perform the deinitialization
+        
+        // Remove the notification
+        // [[NSNotificationCenter defaultCenter] removeObserver:self];
+    }
+
+    
+    func showDateInputDialog(buttonID: String) {
+
+        //Setting title and message for the alert dialog
+        let alertController = UIAlertController(title: "Enter New Date String", message: "Format yyyy-mm-dd", preferredStyle: .alert)
+        
+        //the confirm action taking the inputs
+        let confirmAction = UIAlertAction(title: "Enter", style: .default) { (_) in
+            
+            //getting the input values from user
+            let inputDateString = (alertController.textFields?[0].text)!
+            
+            // validate string
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            
+
+            if dateFormatter.date(from: inputDateString) != nil {
+                
+                if buttonID == "Start" {
+                    self.startDateString = inputDateString
+                } else if buttonID == "End" {
+                    self.endDateString = inputDateString
+                }
+                self.refreshView()
+
+            } else {
+                // invalid format
+                print("invalid date format")
+            }
+            
+        }
+        
+        //the cancel action doing nothing
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        //adding textfields to our dialog box
+        alertController.addTextField { (textField) in
+            textField.placeholder = "yyyy-mm-dd"
+        }
+        
+        //adding the action to dialogbox
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        //finally presenting the dialog box
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func showGoalInputDialog() {
+        //Creating UIAlertController and
+        //Setting title and message for the alert dialog
+        
+        
+        let alertController = UIAlertController(title: "Contest Goal", message: "Please enter a new step goal for this contest.", preferredStyle: .alert)
+        
+        //the confirm action taking the inputs
+        let confirmAction = UIAlertAction(title: "Enter", style: .default) { (_) in
+            
+            //getting the input values from user
+            let inputDateString = (alertController.textFields?[0].text)!
+            self.contestGoalStepCount = Float(inputDateString)!
+            self.refreshView()
+            
+        }
+        
+        //the cancel action doing nothing
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        //adding textfields to our dialog box
+        alertController.addTextField { (textField) in
+            textField.placeholder = "70000"
+        }
+        
+        //adding the action to dialogbox
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        //finally presenting the dialog box
+        self.present(alertController, animated: true, completion: nil)
+    }
+
     
 }
 
